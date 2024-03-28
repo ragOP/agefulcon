76b626b6395639f6225f5b90121aa77c06b62ba5
farish-jamal <farishjamal98@gmail.com>
2024-03-29 00:39:03 +0530

    Home page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Chat from "./components/Chat";
 import Homepage from "./components/Homepage";
+import PageHome from "./components/PageHome";
 
 function App() {
   // test
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<PageHome />} />
         <Route path="/q2med" element={<Homepage />} />
         <Route path="/chatmed" element={<Chat />} />
       </Routes>
