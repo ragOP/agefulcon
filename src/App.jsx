35bac57cc117ac8dde457c4bf551c6d7b263b9c6
farish-jamal <farishjamal98@gmail.com>
2024-03-29 00:41:00 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Chat from "./components/Chat";
 import Homepage from "./components/Homepage";
 import PageHome from "./components/PageHome";
@@ -6,13 +6,13 @@ import PageHome from "./components/PageHome";
 function App() {
   // test
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<PageHome />} />
         <Route path="/q2med" element={<Homepage />} />
         <Route path="/chatmed" element={<Chat />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
