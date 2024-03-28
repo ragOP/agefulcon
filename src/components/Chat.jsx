933d0b2eb96a868da94f51f026f0f31b6eefa631
farish-jamal <farishjamal98@gmail.com>
2024-03-29 00:26:54 +0530

    everything pushed

---
+++ src/components/Chat.jsx
@@ -0,0 +1,13 @@
+const Chat = () => {
+  return (
+    <div>
+      <iframe
+        title="External Content"
+        src="https://ageful-connect.vercel.app/"
+        style={{ width: "100%", height: "100vh", border: "none" }}
+      />
+    </div>
+  );
+};
+
+export default Chat;
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,13 @@
+const Homepage = () => {
+  return (
+    <div>
+      <iframe
+        title="External Content"
+        src="https://benifit-discovery.vercel.app/"
+        style={{ width: "100%", height: "100vh", border: "none" }}
+      />
+    </div>
+  );
+};
+
+export default Homepage;
